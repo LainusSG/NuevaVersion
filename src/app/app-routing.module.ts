128fb4e9ad782dcc27b5b1d42317a7b1865156b3
José Lino santos García <191011022@acatlan.tecnm.mx>
2024-03-07 10:05:03 -0600

    Excel-to-angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EXCELTOANGULARComponent } from './excel-to-angular/excel-to-angular.component';
+import { ExcelToAmgular2Component } from './excel-to-amgular2/excel-to-amgular2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'1', component: EXCELTOANGULARComponent},
+  { path:'2', component: ExcelToAmgular2Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EXCELTOANGULARComponent } from './excel-to-angular/excel-to-angular.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import {MatTableModule} from '@angular/material/table';
+import { ExcelToAmgular2Component } from './excel-to-amgular2/excel-to-amgular2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EXCELTOANGULARComponent,
+    ExcelToAmgular2Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTableModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
